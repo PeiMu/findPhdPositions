7c01ba4bd5ce4b266d40f0950fbe17f54af38392
Pei Mu <ds1231h@126.com>
2022-11-03 22:17:11 +0000

    add cgo/cc-2021/2022 specification

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from spider import Spider
 
-url = 'https://conf.researchr.org/track/cgo-2021/cgo-2021-papers?#event-overview'
+# url = 'https://conf.researchr.org/track/cgo-2021/cgo-2021-papers?#event-overview'
+# url = 'https://conf.researchr.org/track/cgo-2022/cgo-2022-main-conference#event-overview'
+# url = 'https://conf.researchr.org/track/CC-2021/cc-research-papers?#event-overview'
+url = 'https://conf.researchr.org/track/CC-2022/CC-2022-research-papers#event-overview'
 spider = Spider()
 paper_lists = spider.crawl_page(url)
 author_info_lists = []
