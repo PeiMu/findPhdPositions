3f9295ae8907658cdf212533b746d7b4413b15ea
Pei Mu <ds1231h@126.com>
2022-11-03 15:39:56 +0000

    get basic author information

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 from spider import Spider
+import argparse
 
-url = 'https://conf.researchr.org/track/cgo-2021/cgo-2021-papers?#event-overview'
-spider = Spider()
-paper_lists = spider.crawl_page(url)
-author_info_lists = []
-file = 'author_info.txt'
-open(file, "w").close()
-for link in paper_lists:
-    if link.find('/profile/') != -1:
-        author_info_lists = spider.collect_info(link)
-        author_info_lists = list(filter(None, author_info_lists))
-        if len(author_info_lists) > 2:
-            with open(file, 'a') as f:
-                for info in author_info_lists:
-                    f.write(f"{info}\n")
-                f.write(f"\n")
-        print(author_info_lists)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='find your interest projects!')
+    parser.add_argument('-confs', default="cgo, compiler construction", type=str,
+                        help='Which conference would you like to check? '
+                             'Please input "conf1, conf2"')
+    parser.add_argument('-years', default="2021, 2022", type=str,
+                        help='Which years would you like to check? '
+                             'Please input "from, to"')
+    args = parser.parse_args()
+    confs = args.confs
+    # confs = confs.replace(' ', '')
+    conf_list = confs.split(',')
+    years = args.years
+    years = years.replace(' ', '')
+    year_list = years.split(',')
+    if len(year_list) == 1:
+        year_list.append(year_list[0])
+    # url = 'https://conf.researchr.org/track/cgo-2021/cgo-2021-papers?#event-overview'
+    for conf in conf_list:
+        conf_name = conf.split(' ')
+        url_pre = 'https://scholar.google.com/scholar?as_q=&as_epq=&as_oq=&as_eq=&as_occt=any&as_sauthors' \
+                  '=&as_publication='
+        url_year = '&as_ylo='+year_list[0]+'&as_yhi='+year_list[1]+'&hl=en&as_sdt=0%2C5'
+        url_name = ''
+        for c_name in conf_name:
+            if c_name != '':
+                url_name += '+'+c_name
+        url = url_pre + url_name[1:] + url_year
+        spider = Spider()
+        paper_info = spider.collect_paper(url)
+        # paper_lists = spider.crawl_page(url)
+        author_info_map = {'paper title': ['name',
+                                           'affiliation',
+                                           'personal_page',
+                                           'research_interest']}
+        # file = 'author_info.txt'
+        # open(file, "w").close()
+        for link in paper_info:
+            author_info = spider.collect_info(link)
+            author_info = list(filter(None, author_info))
+            if not author_info:
+                print(link, '\t', paper_info[link])
+                continue
+            # if author_info[0] in author_info_map:
+            author_info_map[author_info[0]] = author_info[1:]
+            # if len(author_info_map) > 3:
+            #     with open(file, 'a') as f:
+            #         for info in author_info_map:
+            #             f.write(f"{info}\n")
+            #         f.write(f"\n")
+            print(author_info_map)
--- spider.py
+++ spider.py
@@ -2,6 +2,7 @@ from urllib.request import Request, urlopen
 from link_finder import LinkFinder
 from bs4 import BeautifulSoup
 import re
+from urllib.parse import urlparse
 
 
 class Spider:
@@ -14,9 +15,82 @@ class Spider:
         Spider.add_links_to_queue(Spider.gather_links(page_url))
         return Spider.queue
 
-    # Collect author information
+    # Collect paper information
+    @staticmethod
+    def collect_paper(page_url):
+        try:
+            req = Request(page_url, headers={'User-Agent': 'Mozilla/5.0'})
+            response = urlopen(req)
+            content = response.read()
+            soup = BeautifulSoup(content, 'html.parser')
+        except Exception as e:
+            print(str(e))
+            return set()
+        for links in soup.findAll('span'):
+            links.unwrap()
+        soup_content = str(soup)
+        paper_map = {}
+        paper_re_list = re.findall('data-clk-atid="(.*?)" href="(.*?) id=(.*?)">(.*?)</a>',
+                                   soup_content)
+        for paper_re in paper_re_list:
+            if paper_re is not None:
+                paper_link = paper_re[1]
+                paper_link_re = re.search('href="(.*?)/"', paper_link)
+                if paper_link_re is not None:
+                    paper_link = paper_link_re.group(1)
+                else:
+                    paper_link = paper_link[-1]
+                paper_name = paper_re[3]
+                paper_map.update({paper_link: paper_name})
+        return paper_map
+
+    # Collect author information interface
     @staticmethod
     def collect_info(page_url):
+        if page_url.find('ieee') != -1:
+            return Spider.collect_ieee_info(page_url)
+        else:
+            print("don't support such conference with ", page_url, "yet")
+            return []
+
+    # Collect author information of ieee format
+    @staticmethod
+    def collect_ieee_info(page_url):
+        req = Request(page_url, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urlopen(req)
+        content = response.read()
+        soup = BeautifulSoup(content, 'html.parser')
+        name = ''
+        affiliation = ''
+        author_id = ''
+        for links in soup.findAll('span'):
+            links.unwrap()
+        soup_content = str(soup)
+        author_re = re.search('"authors":(.*?),"isbn"', soup_content)
+        if author_re is not None:
+            author_info = author_re.group(1)
+            name_re = re.search('"name":"(.*?)",', author_info)
+            if name_re is not None:
+                name = name_re.group(1)
+            affiliation_re = re.search('"affiliation":\["(.*?)"\],', author_info)
+            if affiliation_re is not None:
+                affiliation = affiliation_re.group(1)
+            author_id_re = re.search('"id":"(.*?)"', author_info)
+            if author_id_re is not None:
+                author_id = author_id_re.group(1)
+                author_page_link = 'https://' + urlparse(page_url).netloc + '/author/' + author_id
+                author_page_req = Request(author_page_link, headers={'User-Agent': 'Mozilla/5.0'})
+                author_page_response = urlopen(author_page_req)
+                author_page_content = author_page_response.read()
+                author_page_soup = BeautifulSoup(author_page_content, 'html.parser')
+                for links in author_page_soup.findAll('span'):
+                    links.unwrap()
+                author_page_soup_content = str(author_page_soup)
+        return [name, affiliation, author_id]
+
+    # Collect author information of cgo format
+    @staticmethod
+    def collect_cgo_info(page_url):
         req = Request(page_url, headers={'User-Agent': 'Mozilla/5.0'})
         response = urlopen(req)
         content = response.read()
@@ -25,19 +99,32 @@ class Spider:
         affiliation = ''
         personal_page = ''
         research_interest = ''
+        contributed_item = ''
         for links in soup.findAll('span'):
             links.unwrap()
         soup_content = str(soup)
         if soup_content.find('Name') != -1:
-            name = re.search('Name:(.*?)</div>', soup_content).group(1)
+            name_re = re.search('Name:(.*?)</div>', soup_content)
+            if name_re is not None:
+                name = name_re.group(1)
         if soup_content.find('Affiliation') != -1:
-            affiliation = re.search('Affiliation:(.*?)</div>', soup_content).group(1)
+            affiliation_re = re.search('Affiliation:(.*?)</div>', soup_content)
+            if affiliation_re is not None:
+                affiliation = affiliation_re.group(1)
         if soup_content.find('Personal website') != -1:
-            personal_page = re.search('Personal website:<a class="navigate" href="(.*?)">', soup_content).group(1)
+            personal_page_re = re.search('Personal website:<a class="navigate" href="(.*?)">', soup_content)
+            if personal_page_re is not None:
+                personal_page = personal_page_re.group(1)
         if soup_content.find('Research interests') != -1:
-            research_interest = re.search('Research interests:(.*?)</div>', soup_content).group(1)
+            research_interest_re = re.search('Research interests:(.*?)</div>', soup_content)
+            if research_interest_re is not None:
+                research_interest = research_interest_re.group(1)
+        if soup_content.find('Contributed Item') != -1:
+            soup_content_re = re.search('Contributed Item(.*?)" href="#">(.*?)</a>', soup_content)
+            if soup_content_re is not None:
+                contributed_item = soup_content_re.group(2)
 
-        return [name, affiliation, personal_page, research_interest]
+        return [contributed_item, name, affiliation, personal_page, research_interest]
 
     # Converts raw response data into readable information and checks for proper html formatting
     @staticmethod
